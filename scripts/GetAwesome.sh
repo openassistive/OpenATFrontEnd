d943c2169caae1b1ab7fc70334ec190273940645
willwade <willwade@gmail.com>
2017-11-21 09:21:58 +0000

    updated around typo

--- scripts/GetAwesome.sh
+++ scripts/GetAwesome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -o content/post/awesome-assitivetech.md https://raw.githubusercontent.com/openassistive/awesome-assitivetech/master/README.md --silent
+curl -o content/post/awesome-assistivetech.md https://raw.githubusercontent.com/openassistive/awesome-assistivetech/master/README.md --silent
 
 DATEF=`date +%Y-%m-%dT%H:%M:%S.000Z`
 
-echo -e "---\ntitle: Awesome Assitivetech\ndate: $DATEF\nurl: awesome-assitivetech\n---\n$(cat content/post/awesome-assitivetech.md)" > content/post/awesome-assitivetech.md
+echo -e "---\ntitle: Awesome Assistivetech\ndate: $DATEF\nurl: awesome-assistivetech\n---\n$(cat content/post/awesome-assistivetech.md)" > content/post/awesome-assistivetech.md
