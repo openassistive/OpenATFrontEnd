adaa3c29c995d970b86631734d29565ea44cc8b9
willwade <willwade@gmail.com>
2017-11-21 08:58:06 +0000

    should fix build error

--- scripts/GetAwesome.sh
+++ scripts/GetAwesome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -o content/post/awesome-assitivetech.md https://raw.githubusercontent.com/openassistive/awesome-assitivetech/master/README.md --silent
 
-DATEF=`date +%Y-%m-%dT%H:%M:%S%z`
+DATEF=`date +%Y-%m-%dT%H:%M:%S+000Z`
 
 echo -e "---\ntitle: Awesome Assitivetech\ndate: $DATEF\nurl: awesome-assitivetech\n---\n$(cat content/post/awesome-assitivetech.md)" > content/post/awesome-assitivetech.md
